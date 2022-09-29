17bd5bbea5fb4377c69b77183924d7b1fe382e84
mark prima <prima@tekaja.id>
2022-09-29 15:56:34 +0700

    deprecated some code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   def home
+    @username = Balance.all.collect(&:username) 
+    # => ["mark", "prima", "nabil", "akbar"]
   end
-
   def about
   end
 end
